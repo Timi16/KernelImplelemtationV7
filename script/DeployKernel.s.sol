cc81c3d7a4663e9481b5f38b2f986f0f3859323d
Timi16 <timilehinolowu46@gmail.com>
2026-01-26 09:39:40 -0800

    Adding foundry fixes

--- script/DeployKernel.s.sol
+++ script/DeployKernel.s.sol
@@ -29,7 +29,13 @@ contract DeployKernel is Script {
         console2.log("=== Verification Command ===");
         console2.log("forge verify-contract \\");
         console2.log("  --chain-id", block.chainid, "\\");
-        console2.log("  --constructor-args", abi.encode(entrypointAddress), "\\");
+        console2.log(
+            string.concat(
+                "  --constructor-args ",
+                vm.toString(abi.encode(entrypointAddress)),
+                " \\"
+            )
+        );
         console2.log("  --etherscan-api-key no-key-needed \\");
         console2.log("  --verifier-url https://chainscan.0g.ai/api \\");
         console2.log("  ", address(kernel), "\\");
