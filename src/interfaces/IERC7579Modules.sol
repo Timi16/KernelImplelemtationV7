9eeb075584757941c0338da7ad3723fc2d343cae
Timi16 <timilehinolowu46@gmail.com>
2026-02-04 18:11:04 -0800

    Adding Fixes to modules

--- src/interfaces/IERC7579Modules.sol
+++ src/interfaces/IERC7579Modules.sol
@@ -44,8 +44,8 @@ interface IValidator is IModule {
 
     /**
      * @dev Validates a transaction on behalf of the account.
-     *         This function is intended to be called by the MSA during the ERC-4337 validaton phase
-     *         Note: solely relying on bytes32 hash and signature is not suffcient for some
+     *         This function is intended to be called by the MSA during the ERC-4337 validation phase
+     *         Note: solely relying on bytes32 hash and signature is not sufficient for some
      * validation implementations (i.e. SessionKeys often need access to userOp.calldata)
      * @param userOp The user operation to be validated. The userOp MUST NOT contain any metadata.
      * The MSA MUST clean up the userOp before sending it to the validator.
@@ -69,30 +69,12 @@ interface IValidator is IModule {
 interface IExecutor is IModule {}
 
 interface IHook is IModule {
-    /**
-     * @dev Called by the smart account before execution
-     * @param msgSender the address that called the smart account
-     * @param value the value that was sent to the smart account
-     * @param msgData the data that was sent to the smart account
-     *
-     * MAY return arbitrary data in the `hookData` return value
-     */
-    function preCheck(address msgSender, uint256 value, bytes calldata msgData)
+    function preCheck(address msgSender, uint256 msgValue, bytes calldata msgData)
         external
         payable
         returns (bytes memory hookData);
 
-    /**
-     * @dev Called by the smart account after execution
-     * @param hookData the data that was returned by the `preCheck` function
-     * @param executionSuccess whether the execution(s) was (were) successful
-     * @param executionReturn the return/revert data of the execution(s)
-     *
-     * MAY validate the `hookData` to validate transaction context of the `preCheck` function
-     */
-    function postCheck(bytes calldata hookData, bool executionSuccess, bytes calldata executionReturn)
-        external
-        payable;
+    function postCheck(bytes calldata hookData) external payable;
 }
 
 interface IFallback is IModule {}
